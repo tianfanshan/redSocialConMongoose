1c0d4efd76b2bf3484cb303f36098390b44cb0ad
tianfanshan <stf30111994@gmail.com>
2022-05-31 13:39:51 +0200

    update user controller hecho

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,14 +10,15 @@ const UserController = {
             res.status(500).send({message:"Ha habido un problema al crear el usuario"})
         }
     },
-    async upDate(req,res){
+    async update(req,res){
         try {
             const user = await User.findByIdAndUpdate(req.params._id,req.body,{new:true});
             res.send({message:"Usuario actualizado con éxito",user});
         } catch (error) {
             console.error(error)
         }
-    }
+    },
+
 }
 
 module.exports = UserController;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/UserController')
-const User = require('../models/User')
 
 router.post('/',UserController.create);
+router.put('/id/:_id',UserController.update);
 
 module.exports = router;
