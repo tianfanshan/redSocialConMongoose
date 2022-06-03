d43ec8bf8d71644da928458594dcc0b0583afc29
tianfanshan <stf30111994@gmail.com>
2022-06-03 12:43:30 +0200

    archivo config database

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -3,13 +3,12 @@ const Comment = require('../models/Comment')
 const CommentController = {
     async create(req,res){
         try {
-            const comment = await Comment.findById(req.params._id
-                
-                // ...req.body,
-                // daliveryDate:new Date(),
-                // userId:req.user._id
-            )
-
+            const comment = await Comment.create({
+                ...req.body,
+                daliveryDate:new Date(),
+                userId:req.user._id,
+                postId:req.params._id
+            })
             res.status(201).send({message:'Comentario creado',comment})
         } catch (error) {
             console.error(error)
