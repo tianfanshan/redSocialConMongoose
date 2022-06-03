b40fce79d17d8dca80ec8a11c6eb74738e39ed27
tianfanshan <stf30111994@gmail.com>
2022-06-03 12:23:47 +0200

    Este commit es de Ger <3

---
+++ config/config.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const MONGO_URI = process.env.MONGO_URI;
+
+const dbConnection = async()=>{
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('Base de datos conectada con éxito!');
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error a la hora de iniciar la base de datos.')
+    }
+};
+
+
+
+module.exports = {
+    dbConnection
+};
